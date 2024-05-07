6629f672fdc78816d2f844326372305694317672
cmmasaba <mmasabacollins9@gmail.com>
2024-05-07 20:53:09 +0300

    Configred the list view for books

--- books/views.py
+++ books/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Book
+
+class BooksListView(ListView):
+    model = Book
+    template_name = 'books/books_list.html'
